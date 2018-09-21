000fa356436bfbeed3817c6926ebb10e5da5e7e2
Nicky Chen <nicky.chen0@gmail.com>
2018-09-21 15:55:20 -0700

    bst's

--- Binary Search Trees/BST_Intro.py
+++ Binary Search Trees/BST_Intro.py
@@ -12,13 +12,92 @@ class BinarySearchTree:
 # insert: insert adds the input value to the binary search tree
     def insert(self, value):
         node = BinarySearchTree(value)
-        if not self.value:
-            self.value = node
+        if not self:
+            self = node
+        # To prevent against duplicates.
+        if self.value == node.value:
+            return
         else:
             if value < self.value:
-                self.left.insert(value)
+                if not self.left:
+                    self.left = node
+                else:
+                    self.left.insert(value)
+            if value > self.value:
+                if not self.right:
+                    self.right = node
+                else:
+                    self.right.insert(value)
+
+# search: If target is in the tree, return True, else return False
+    def search(self, target):
+        if self.value == target:
+            return True
+        else:
+            if target < self.value:
+                return self.left.search(target)
+            if target > self.value:
+                return self.right.search(target)
+        return False
+
+
+# insert out of class: (node is already a node, not a value).
+def insert(root, node):
+    # Code here
+    if not root:
+        root = node
+    if root.value == node.value:
+        return
+    else:
+        if node.value < root.value:
+            if not root.left:
+                root.left = node
             else:
-                self.right.insert(value)
+                insert(root.left, node)
+        else:
+            if not root.right:
+                root.right = node
+            else:
+                insert(root.right, node)
+
+# insertion with Node & BinarySearchTree Class:
 
 
-bst = BinarySearchTree(5)
+class Node:
+    def __init__(self, info):
+        self.info = info
+        self.left = None
+        self.right = None
+        self.level = None
+
+    def __str__(self):
+        return str(self.info)
+
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, val):
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            self._insert(val, self.root)
+        # Enter you code here.
+
+    def _insert(self, val, node):
+        if val < node.info:
+            if node.left == None:
+                node.left = Node(val)
+            else:
+                self._insert(val, node.left)
+        elif val > node.info:
+            if node.right == None:
+                node.right = Node(val)
+            else:
+                self._insert(val, node.right)
+
+        else:
+            print("value already in tree")
+
+# deletion
---
+++ Binary Search Trees/LC - Search in BST.py
@@ -0,0 +1,21 @@
+# 700. Search in a Binary Search Tree
+# Given the root node of a binary search tree (BST) and a value. You need to find the node in the BST that the node's value equals the given value. Return the subtree rooted with that node. If such node doesn't exist, you should return NULL.
+
+
+class Solution:
+    def searchBST(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        """
+        if root is None:
+            return []
+        if root.val == val:
+            return root
+        else:
+            if val < root.val:
+                return self.searchBST(root.left, val)
+            if val > root.val:
+                return self.searchBST(root.right, val)
+        return []
